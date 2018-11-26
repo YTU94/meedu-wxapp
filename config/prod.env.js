8ad49e64b82fa54b05027db8e18205ca55575d10
ytu94 <453980450@qq.com>
2018-11-26 22:34:15 +0800

    修改配置信息

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"production"',
-  API: 'https://1o1.cc',
-  client_id: '2',
-  client_secret: 'G8hmzjkkJBl9lPwF45pBgO1AJSM5XolpbPNFR9k7'
+  API: '"https://58hualong.com"',
+  client_id: '"4"',
+  client_secret: '"ySVIM8phjzVO5XdEZWNXyMHwjo5n23MmpVfi2Xs9"'
 }
