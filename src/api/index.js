bf12b0c7de98ae2a7b9b4f206c34e17e92b7ebe6
linyi <linyi@tsign.cn>
2019-03-30 15:47:00 +0800

    @feat 删除faq模块

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
-import article from './article'
 import course from './course'
 import user from './user'
 import video from './video'
 import vip from './vip'
 
 export default {
-  article,
   course,
   user,
   video,
