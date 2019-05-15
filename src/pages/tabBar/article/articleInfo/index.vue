633fa3fdac6ada3e3524ad50e7452d20f04a5df0
linyi <linyi@tsign.cn>
2019-05-15 16:20:30 +0800

    prep: 优化索索UI&&完善文章显示

--- src/pages/tabBar/article/articleInfo/index.vue
+++ src/pages/tabBar/article/articleInfo/index.vue
@@ -1,65 +1,69 @@
 <template>
-  <div class="article-info">
-    <p class="article-info_title">{{title || '标题'}}</p>
-    <p class="article-info_content" v-html="content || 'markdown text'"></p>
-    <p class="article-note">博客原文：http://ytuj.cn </p>
-  </div>
+    <div class="article-info">
+        <p class="article-info_title">{{title || '标题'}}</p>
+        <wx-parse :content="content"/>
+        <p class="article-note">博客原文：http://ytuj.cn</p>
+    </div>
 </template>
 
 <script>
 import { formatTime } from "@/utils/index";
 import card from "@/components/card";
+import wxParse from "mpvue-wxparse";
 
 export default {
-  components: {
-    card
-  },
+    components: {
+        card,
+        wxParse
+    },
 
-  data() {
-    return {
-      title: "",
-      content: "",
-      articleInfo: {}
-    };
-  },
-  computed: {
-    articleId() {
-      return this.$mp.query.id;
+    data() {
+        return {
+            title: "",
+            content: "",
+            articleInfo: {}
+        };
+    },
+    computed: {
+        articleId() {
+            return this.$mp.query.id;
+        }
+    },
+    methods: {
+        init() {
+            this.title = this.$mp.query.title;
+            this.content = wx.getStorageSync("curPostContent");
+            wx.removeStorage({
+                key: "curPostContent,"
+            });
+        }
+    },
+    onShow() {
+        this.init();
     }
-  },
-  methods: {
-    init() {
-      this.title = this.$mp.query.title;
-      this.content = wx.getStorageSync("curPostContent");
-      wx.removeStorage({
-        key: "curPostContent,"
-      });
-    }
-  },
-  onShow() {
-    this.init();
-  }
 };
 </script>
 
 <style lang="less">
+@import url("~mpvue-wxparse/src/wxParse.css");
+
 .article-info {
-  display: flex;
-  flex-direction: column;
-  padding: 40rpx;
-  &_title {
-    font-size: 32rpx;
-    color: #1a1a1a;
-  }
-  &_content {
-    font-size: 28rpx;
-    color: #777;
-  }
-  .article-note{
-    font-size: 28rpx;
-    padding: 40rpx 0;
-    text-align: center;
-    color: @font-color-gray;
-  }
+    display: flex;
+    flex-direction: column;
+    padding: 40rpx;
+    &_title {
+        font-size: 32rpx;
+        color: #1a1a1a;
+    }
+    &_content {
+        font-size: 28rpx;
+        color: #777;
+    }
+    .article-note {
+        font-size: 28rpx;
+        padding: 40rpx 0;
+        text-align: center;
+        color: @font-color-gray;
+    }
 }
 </style>
