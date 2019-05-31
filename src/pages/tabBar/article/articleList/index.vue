36c2b6410a5641326dc309291840b8ede3990ffe
linyi <linyi@tsign.cn>
2019-05-31 12:55:34 +0800

    @prep: 优化文章加载loading

--- src/pages/tabBar/article/articleList/index.vue
+++ src/pages/tabBar/article/articleList/index.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="article-list">
-    <p>文章列表</p>
-    <ul class="article-list-ul">
-      <li
-        class="list-item"
-        v-for="(item, index) in articleList"
-        @click="goArticleInfo(item)"
-        :class="{ red: aa }"
-        :key="index"
-      >
-        <div class="list-item_title">{{item.post_title}}</div>
-        <div class="list-item_info">
-          <span>作者：{{item.display_name}}</span>
-          <span>{{item.post_date}}</span>
-        </div>
-      </li>
-    </ul>
-  </div>
+    <div class="article-list">
+        <p>文章列表</p>
+        <ul class="article-list-ul">
+            <li
+                class="list-item"
+                v-for="(item, index) in articleList"
+                @click="goArticleInfo(item)"
+                :class="{ red: aa }"
+                :key="index"
+            >
+                <div class="list-item_title">{{item.post_title}}</div>
+                <div class="list-item_info">
+                    <span>作者：{{item.display_name}}</span>
+                    <span>{{item.post_date}}</span>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -24,60 +24,68 @@ import { formatTime } from "@/utils/index";
 import card from "@/components/card";
 
 export default {
-  components: {
-    card
-  },
+    components: {
+        card
+    },
 
-  data() {
-    return {
-      categoryId: "",
-      articleList: []
-    };
-  },
-  methods: {
-    init() {
-      this._getArticleList({ page_size: 10, page: 1 }, this.categoryId);
+    data() {
+        return {
+            categoryId: "",
+            articleList: []
+        };
     },
-    goArticleInfo(e) {
-      wx.navigateTo({
-        url: `../articleInfo/main?id=${e.ID}&title=${e.post_title}`
-      });
-      wx.setStorageSync("curPostContent", e.post_content);
-    },
-    // 获取文章列表
-    _getArticleList(data, id) {
-      const that = this;
-      wx.request({
-        url: "https://api.ytuj.cn/api/v1/ytu/articles", // 仅为示例，并非真实的接口地址
-        data: {
-          page: 1,
-          page_size: 10,
-          category_id: this.categoryId
+    methods: {
+        init() {
+            this._getArticleList({ page_size: 10, page: 1 }, this.categoryId);
         },
-        method: "GET",
-        header: {
-          "content-type": "application/json" // 默认值
+        goArticleInfo(e) {
+            wx.navigateTo({
+                url: `../articleInfo/main?id=${e.ID}&title=${e.post_title}`
+            });
+            wx.setStorageSync("curPostContent", e.post_content);
         },
-        success(res) {
-          that.articleList = res.data.data.concat(that.articleList);
-          that.articleList.forEach(e => {
-            e.post_date = e.post_date.slice(0, 10);
-          });
+        // 获取文章列表
+        _getArticleList(data, id) {
+            const that = this;
+            wx.showLoading({
+                title: "加载中",
+                mask: true
+            });
+            wx.request({
+                url: "https://api.ytuj.cn/api/v1/ytu/articles", // 仅为示例，并非真实的接口地址
+                data: {
+                    page: 1,
+                    page_size: 10,
+                    category_id: this.categoryId
+                },
+                method: "GET",
+                header: {
+                    "content-type": "application/json" // 默认值
+                },
+                success(res) {
+                    wx.hideLoading();
+                    that.articleList = res.data.data.concat(that.articleList);
+                    that.articleList.forEach(e => {
+                        e.post_date = e.post_date.slice(0, 10);
+                    });
+                },
+                fail() {
+                    wx.hideLoading();
+                }
+            });
         }
-      });
+    },
+    mounted() {},
+    onShow() {
+        this.categoryId = this.$mp.query.id;
+    },
+    onReady() {
+        this.init();
+    },
+    onHide() {},
+    onUnload() {
+        this.articleList = [];
     }
-  },
-  mounted() {},
-  onShow() {
-    this.categoryId = this.$mp.query.id;
-  },
-  onReady() {
-    this.init();
-  },
-  onHide() {},
-  onUnload() {
-    this.articleList = [];
-  }
 };
 </script>
 
@@ -85,36 +93,36 @@ export default {
 @import "../../../../assets/style/variable";
 
 .article-list {
-  padding: 0 20px;
-  box-sizing: border-box;
-  font-size: 16px;
-  p {
+    padding: 0 20px;
+    box-sizing: border-box;
     font-size: 16px;
-  }
-  .article-list-ul {
-    display: flex;
-    flex-direction: column;
-    font-size: 14px;
-    padding: 20px 0;
-    .list-item {
-      flex: 1;
-      width: 100%;
-      line-height: 2;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      white-space: nowrap;
-      border-bottom: 1px solid @border-color;
-      &_title {
-        font-size: 28rpx;
-        color: #1a1a1a;
-      }
-      &_info {
-        display: flex;
-        justify-content: space-between;
-        font-size: 24rpx;
-        color: @font-color-gray;
-      }
+    p {
+        font-size: 16px;
     }
-  }
+    .article-list-ul {
+        display: flex;
+        flex-direction: column;
+        font-size: 14px;
+        padding: 20px 0;
+        .list-item {
+            flex: 1;
+            width: 100%;
+            line-height: 2;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            white-space: nowrap;
+            border-bottom: 1px solid @border-color;
+            &_title {
+                font-size: 28rpx;
+                color: #1a1a1a;
+            }
+            &_info {
+                display: flex;
+                justify-content: space-between;
+                font-size: 24rpx;
+                color: @font-color-gray;
+            }
+        }
+    }
 }
 </style>
--- src/pages/tabBar/article/index.vue
+++ src/pages/tabBar/article/index.vue
@@ -1,68 +1,76 @@
 <template>
-  <div class="article-list">
-    <p>所有分类</p>
-    <ul class="article-list-ul">
-      <li
-        class="list-item"
-        v-for="(item, index) in articleList"
-        @click="goArticleInfo(item)"
-        :class="{ red: aa }"
-        :key="index"
-      >
-        <div class="list-item_title">{{item.name}}</div>
-        <div class="list-item_info">
-          <span>{{item.description || '暂无描述'}}</span>
-          <span>{{item.count}}篇文章</span>
-        </div>
-      </li>
-    </ul>
-  </div>
+    <div class="article-list">
+        <p>所有分类</p>
+        <ul class="article-list-ul">
+            <li
+                class="list-item"
+                v-for="(item, index) in articleList"
+                @click="goArticleInfo(item)"
+                :class="{ red: aa }"
+                :key="index"
+            >
+                <div class="list-item_title">{{item.name}}</div>
+                <div class="list-item_info">
+                    <span>{{item.description || '暂无描述'}}</span>
+                    <span>{{item.count}}篇文章</span>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 import { formatTime } from "@/utils/index";
 
 export default {
-  components: {},
+    components: {},
 
-  data() {
-    return {
-      articleList: []
-    };
-  },
-  methods: {
-    init() {
-      this._getArticleList({ page_size: 10, page: 1 });
+    data() {
+        return {
+            articleList: []
+        };
     },
-    goArticleInfo(e) {
-      wx.navigateTo({
-        url: `../article/articleList/main?id=${e.term_id}`
-      });
-      wx.setStorageSync("curPostContent", e.post_content);
-    },
-    // 获取文章列表
-    _getArticleList(data) {
-      const that = this;
-      wx.request({
-        url: "https://api.ytuj.cn/api/v1/ytu/category",
-        data: {
-          page: 1,
-          page_size: 10
+    methods: {
+        init() {
+            this._getArticleList({ page_size: 10, page: 1 });
         },
-        method: "GET",
-        header: {
-          "content-type": "application/json" // 默认值
+        goArticleInfo(e) {
+            wx.navigateTo({
+                url: `../article/articleList/main?id=${e.term_id}`
+            });
+            wx.setStorageSync("curPostContent", e.post_content);
         },
-        success(res) {
-          that.articleList = res.data.data;
+        // 获取文章列表
+        _getArticleList(data) {
+            const that = this;
+            wx.showLoading({
+                title: "加载中",
+                mask: true
+            });
+            wx.request({
+                url: "https://api.ytuj.cn/api/v1/ytu/category",
+                data: {
+                    page: 1,
+                    page_size: 10
+                },
+                method: "GET",
+                header: {
+                    "content-type": "application/json" // 默认值
+                },
+                success(res) {
+                    that.articleList = res.data.data;
+                    wx.hideLoading();
+                },
+                fail() {
+                    wx.hideLoading();
+                }
+            });
         }
-      });
+    },
+    mounted() {},
+    onShow() {
+        this.init();
     }
-  },
-  mounted() {},
-  onShow() {
-    this.init();
-  }
 };
 </script>
 
@@ -70,37 +78,37 @@ export default {
 @import "../../../assets/style/variable";
 
 .article-list {
-  padding: 0 20px;
-  box-sizing: border-box;
-  font-size: 16px;
-  p {
+    padding: 0 20px;
+    box-sizing: border-box;
     font-size: 16px;
-  }
-  .article-list-ul {
-    display: flex;
-    flex-direction: column;
-    font-size: 14px;
-    padding: 20px 0;
-    .list-item {
-      flex: 1;
-      width: 100%;
-      line-height: 2;
-      text-overflow: ellipsis;
-      overflow: hidden;
-      white-space: nowrap;
-      padding: 10rpx 0;
-      border-bottom: 1px solid @border-color;
-      &_title {
-        font-size: 28rpx;
-        color: #1a1a1a;
-      }
-      &_info {
-        display: flex;
-        justify-content: space-between;
-        font-size: 24rpx;
-        color: @font-color-gray;
-      }
+    p {
+        font-size: 16px;
     }
-  }
+    .article-list-ul {
+        display: flex;
+        flex-direction: column;
+        font-size: 14px;
+        padding: 20px 0;
+        .list-item {
+            flex: 1;
+            width: 100%;
+            line-height: 2;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            white-space: nowrap;
+            padding: 10rpx 0;
+            border-bottom: 1px solid @border-color;
+            &_title {
+                font-size: 28rpx;
+                color: #1a1a1a;
+            }
+            &_info {
+                display: flex;
+                justify-content: space-between;
+                font-size: 24rpx;
+                color: @font-color-gray;
+            }
+        }
+    }
 }
 </style>
